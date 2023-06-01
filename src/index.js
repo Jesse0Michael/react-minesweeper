08850d6caf2caab76c9af9eb80e893817805fe25
Jesse Michael <jesse.michael1234@gmail.com>
2023-06-01 12:34:47 -0700

    fix: use GA4

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
-ReactGA.initialize('UA-25334252-4');
+ReactGA.initialize('G-NLEF226982');
 ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
